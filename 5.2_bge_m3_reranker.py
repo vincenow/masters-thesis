6d4f0f0716330932ffa3ee68b7828684a3b30be4
vincenow <v.hagenow@bluewin.ch>
2026-04-24 17:37:08 +0200

    batch_size=2

--- 5.2_bge_m3_reranker.py
+++ 5.2_bge_m3_reranker.py
@@ -75,7 +75,7 @@ def run_condition(language, language_name, label_lang, label_condition):
     doc_embeddings = embedding_model.encode(
         texts,
         show_progress_bar=True,
-        batch_size=8
+        batch_size=2
     )
 
     k_values = [5, 10, 20, 50, 100]
